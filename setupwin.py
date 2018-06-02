00d9d8cf962a92bd980a84a4a4eca42768e4acce
Seckin Can Sahin <seckincansahin@gmail.com>
2018-06-01 21:07:52 -0700

    numpy added to setupwin

--- setupwin.py
+++ setupwin.py
@@ -4,7 +4,7 @@ setup(
     name = "coinium",
     version = "1.0.0",
     options = {"build_exe": {
-        'packages': ["wx", "os","sys","ctypes","win32con"],
+        'packages': ["wx", "os","sys","ctypes","win32con", "numpy"],
         'include_msvcr': True,
     }},
     executables = [Executable("obfuscated_graph.py",base="Win32GUI")]
