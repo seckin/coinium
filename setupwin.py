da42f5d44ca108c4f712993ea73568caa7e8d4b3
Seckin Can Sahin <seckincansahin@gmail.com>
2018-06-01 18:31:28 -0700

    setupwin.py initial version

---
+++ setupwin.py
@@ -0,0 +1,11 @@
+#setup.py
+from cx_Freeze import setup, Executable
+setup(
+    name = "coinium",
+    version = "1.0.0",
+    options = {"build_exe": {
+        'packages': ["os","sys","ctypes","win32con"],
+        'include_msvcr': True,
+    }},
+    executables = [Executable("obfuscated_graph.py",base="Win32GUI")]
+    )
