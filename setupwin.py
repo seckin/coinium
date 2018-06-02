56fb6916cd6b622c578ac61dd4a4131af7c9b8de
Seckin Can Sahin <seckincansahin@gmail.com>
2018-06-01 22:54:40 -0700

    adding tcl dlls to setupwin.py

--- setupwin.py
+++ setupwin.py
@@ -1,9 +1,18 @@
 #setup.py
 from cx_Freeze import setup, Executable
+import os.path
+PYTHON_INSTALL_DIR = os.path.dirname(os.path.dirname(os.__file__))
+os.environ['TCL_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tcl8.6')
+os.environ['TK_LIBRARY'] = os.path.join(PYTHON_INSTALL_DIR, 'tcl', 'tk8.6')
+
 setup(
     name = "coinium",
     version = "1.0.0",
     options = {"build_exe": {
+        'include_files':[
+            os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tk86t.dll'),
+            os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tcl86t.dll'),
+         ],
         'packages': ["wx", "os","sys","ctypes","win32con", "numpy"],
         'include_msvcr': True,
     }},
