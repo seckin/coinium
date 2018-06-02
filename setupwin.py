4a0bfaf116c216f3789b0cb2f7c6d895122fa67a
Seckin Can Sahin <seckincansahin@gmail.com>
2018-06-01 18:52:19 -0700

    wx added to setupwin.py

--- setupwin.py
+++ setupwin.py
@@ -4,7 +4,7 @@ setup(
     name = "coinium",
     version = "1.0.0",
     options = {"build_exe": {
-        'packages': ["os","sys","ctypes","win32con"],
+        'packages': ["wx", "os","sys","ctypes","win32con"],
         'include_msvcr': True,
     }},
     executables = [Executable("obfuscated_graph.py",base="Win32GUI")]
