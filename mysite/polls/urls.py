1dab201acc23170da4492238cef18da5dede729a
Seckin Can Sahin <seckincansahin@gmail.com>
2018-06-10 14:29:17 +0300

    mysite initial commit - with example portfolio page

---
+++ mysite/polls/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:pk>/portfolio/', views.PortfolioView.as_view(), name='portfolio'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
