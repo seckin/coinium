b9b79180a39e44f5e4707831b402477bd76c8704
Seckin Can Sahin <seckincansahin@gmail.com>
2018-06-11 20:10:00 +0300

    product page initial commit

--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import include, path
 from django.contrib.auth import views as auth_views
 from django.conf.urls import url
+from . import views
 
 urlpatterns = [
+    url(r'^$', views.HomeView.as_view(), name='home'),
     path('polls/', include('polls.urls')),
     url(r'^login/$', auth_views.login, name='login'),
     url(r'^logout/$', auth_views.logout, {'next_page': '/login'}, name='logout'),
---
+++ mysite/mysite/views.py
@@ -0,0 +1,12 @@
+from django.http import HttpResponse, HttpResponseRedirect
+from django.template import loader
+from django.shortcuts import get_object_or_404, render
+from django.utils import timezone
+from django.views import generic
+from django.urls import reverse
+
+from polls.models import Choice, Question
+
+class HomeView(generic.ListView):
+    model = Question
+    template_name = 'mysite/home.html'
