df73448c3cc314f6e533a2ef3c1928b07924f039
Seckin Can Sahin <seckincansahin@gmail.com>
2018-06-13 15:05:35 +0300

    portfolio model and admin functionality

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth.models import User
 
-from .models import Choice, Question
+from .models import Choice, Question, Portfolio
 
+# class UserInline(admin.TabularInline):
+#     model = User
+#     extra = 1
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
@@ -17,5 +21,16 @@ class QuestionAdmin(admin.ModelAdmin):
     list_filter = ['pub_date']
     search_fields = ['question_text']
 
+class PortfolioAdmin(admin.ModelAdmin):
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        super().save_model(request, obj, form, change)
+    # list_display = ('question_text', 'pub_date', 'was_published_recently')
+    # list_filter = ['pub_date']
+    # inlines = [UserInline]
+    list_display = ('portfolio_name', 'owner', 'btc_pct', 'eth_pct', 'xrp_pct', 'xlm_pct', "created_date")
+    search_fields = ['portfolio_name']
+
 
 admin.site.register(Question, QuestionAdmin)
+admin.site.register(Portfolio, PortfolioAdmin)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,6 +1,7 @@
 import datetime
 from django.db import models
 from django.utils import timezone
+from django.contrib.auth.models import User
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
@@ -17,3 +18,14 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
+
+class Portfolio(models.Model):
+    portfolio_name = models.CharField(max_length=200)
+    owner = models.ForeignKey(User, editable=False, on_delete=models.PROTECT)
+    btc_pct = models.DecimalField(max_digits=5, decimal_places=2)
+    eth_pct = models.DecimalField(max_digits=5, decimal_places=2)
+    xrp_pct = models.DecimalField(max_digits=5, decimal_places=2)
+    xlm_pct = models.DecimalField(max_digits=5, decimal_places=2)
+    created_date = models.DateTimeField('date created')
+    def __str__(self):
+        return self.portfolio_name
