1136bda37882a70b59777ff9992f2c10cfd36f2b
Seckin Can Sahin <seckincansahin@gmail.com>
2018-06-16 17:43:13 +0300

    changed email activation flow. email small style changes

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -58,7 +58,7 @@ def activate(request, uidb64, token):
         user.is_active = True
         user.save()
         login(request, user)
-        # return redirect('home')
-        return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
+        return redirect("/polls/portfolio/1")
+        # return HttpResponse('Thank you for your email confirmation. Now you can login your account.')
     else:
         return HttpResponse('Activation link is invalid!')
