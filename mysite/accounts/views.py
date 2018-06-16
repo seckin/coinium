89113036757712fbc4325cd1456f6b6720eb1360
Seckin Can Sahin <seckincansahin@gmail.com>
2018-06-16 15:46:39 +0300

    auto-login, account verif. emails are sent

--- mysite/accounts/views.py
+++ mysite/accounts/views.py
@@ -24,24 +24,25 @@ def signup(request):
         form = SignupForm(request.POST)
         if form.is_valid():
             user = form.save(commit=False)
-            user.is_active = True#False
+            user.is_active = True
             user.save()
             current_site = get_current_site(request)
-            # mail_subject = 'Activate your coinium account.'
-            # message = render_to_string('acc_active_email.html', {
-            #     'user': user,
-            #     'domain': current_site.domain,
-            #     'uid':urlsafe_base64_encode(force_bytes(user.pk)).decode(),
-            #     'token':account_activation_token.make_token(user),
-            # })
-            # to_email = form.cleaned_data.get('email')
-            # email = EmailMessage(
-            #             mail_subject, message, to=[to_email]
-            # )
-            # email.send()
+            mail_subject = 'Activate your coinium account.'
+            message = render_to_string('acc_active_email.html', {
+                'user': user,
+                'domain': current_site.domain,
+                'uid':urlsafe_base64_encode(force_bytes(user.pk)).decode(),
+                'token':account_activation_token.make_token(user),
+            })
+            to_email = form.cleaned_data.get('email')
+            email = EmailMessage(
+                        mail_subject, message, 'info@coinium.app', to=[to_email], reply_to=['info@coinium.app'],
+            )
+            email.send()
             request.session['just_signed_up'] = True
-            user.is_active = True
-            return redirect("/login")
+            # user.is_active = True
+            login(request, user)
+            return redirect("/polls/portfolio/1")
             # return HttpResponse('Please confirm your email address to complete the registration')
     else:
         form = SignupForm()
