a19a00c25563730ae75f61be8bc634144af42899
Seckin Can Sahin <seckincansahin@gmail.com>
2018-06-20 19:19:15 +0300

    gunicorn changes for release script

--- release script.sh
+++ release script.sh
@@ -1,3 +1,5 @@
+su seckin
+
 echo "export VIRTUALENVWRAPPER_PYTHON=/usr/bin/python3" >> ~/.bashrc
 echo "export WORKON_HOME=~/Env" >> ~/.bashrc
 echo "source /usr/local/bin/virtualenvwrapper.sh" >> ~/.bashrc
@@ -14,8 +16,9 @@ rm -rf ~/coinium
 rm -rf ~/mysite
 git clone https://seckin:co1n23im@github.com/seckin/coinium.git
 mv coinium/mysite/ ./
+rm -rf ~/coinium
 
-printf "\n\nDATABASES = {'default': {'ENGINE': 'django.db.backends.mysql','OPTIONS': {'read_default_file': '/root/mysql.cnf',},}}" >> ~/mysite/mysite/settings.py
+printf "\n\nDATABASES = {'default': {'ENGINE': 'django.db.backends.mysql','OPTIONS': {'read_default_file': '/home/seckin/mysql.cnf',},}}" >> ~/mysite/mysite/settings.py
 
 cd ~/mysite
 ~/mysite/manage.py migrate
@@ -29,8 +32,15 @@ printf '\n\nSPREADS_DB_NAME = "coinim"' >> ~/mysite/mysite/settings.py
 
 ~/mysite/manage.py collectstatic
 
-sudo ufw allow 8080
+#sudo ufw allow 8080
 #~/mysite/manage.py runserver 0.0.0.0:8080
 
-uwsgi --http :8080 --home /root/Env/mysite --chdir /root/mysite --wsgi-file /root/mysite/mysite/wsgi.py
+# uwsgi --http :8080 --home /root/Env/mysite --chdir /root/mysite --wsgi-file /root/mysite/mysite/wsgi.py
+
+# pip3 install django gunicorn psycopg2
+# (mysite) seckin@amineacoin:~/mysite$ /home/seckin/Env/mysite/bin/gunicorn --bind 0.0.0.0:8000 mysite.wsgi
+#(mysiteenv) root@amineacoin:~/mysite# gunicorn --bind 0.0.0.0:8000 mysite.wsgi
+
+sudo killall gunicorn
+/home/seckin/gunicorn_script.sh
 
