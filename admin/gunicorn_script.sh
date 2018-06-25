8f21cc02807e8753a8c1b7f9a21a17bd06492c6b
Seckin Can Sahin <seckincansahin@gmail.com>
2018-06-25 23:24:11 +0300

    fixing investment amount calc logic, gunicorn_script.sh updated for logging

--- admin/gunicorn_script.sh
+++ admin/gunicorn_script.sh
@@ -1,4 +1,4 @@
 cd /home/seckin/
 workon mysite
 cd /home/seckin/mysite
-/home/seckin/Env/mysite/bin/gunicorn --access-logfile - --workers 8 --bind unix:/home/seckin/mysite/mysite.sock mysite.wsgi:application &>/var/log/gunicorn_stdout_stderr.log &
+/home/seckin/Env/mysite/bin/gunicorn --access-logfile - --error-logfile /var/log/gunicorn.error.log --log-file /var/log/gunicorn.stdout.log --workers 3 --bind unix:/home/seckin/mysite/mysite.sock mysite.wsgi:application &
