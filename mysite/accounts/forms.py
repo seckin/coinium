ef37dfc57a510d2d870d6256385274d042ca3fab
Seckin Can Sahin <seckincansahin@gmail.com>
2018-07-01 18:54:54 -0700

    getting and using fullnames

--- mysite/accounts/forms.py
+++ mysite/accounts/forms.py
@@ -6,4 +6,4 @@ class SignupForm(UserCreationForm):
     email = forms.EmailField(max_length=200, help_text='Required')
     class Meta:
         model = User
-        fields = ('username', 'email', 'password1', 'password2')
+        fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2')
