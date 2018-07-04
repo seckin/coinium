9bd08fc5473ee61b93133759c9a70ec95c0ca5ea
Seckin Can Sahin <seckincansahin@gmail.com>
2018-07-03 19:18:57 -0700

    pct gains on left, disabling silk

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'accounts',
     'background_task',
-    'silk',
 ]
 
 MIDDLEWARE = [
@@ -52,7 +51,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'silk.middleware.SilkyMiddleware',
 ]
 
 ROOT_URLCONF = 'mysite.urls'
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -37,4 +37,4 @@ urlpatterns = [
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
-urlpatterns += [url(r'^silk/', include('silk.urls', namespace='silk'))]
+# urlpatterns += [url(r'^silk/', include('silk.urls', namespace='silk'))]
